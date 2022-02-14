6a4fc8f47e540de32cade527a6115412edc47d37
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-14 23:15:17 +0300

    program is done

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("sayi gir: ");
+            int a = int.Parse(Console.ReadLine());
+            int sayi = 1;
+            while (sayi <= a)
+            {
+                Console.Write(a - sayi + 1);
+                sayi++;
+            }
+            Console.Write("...");
+        }
+    }
+}
